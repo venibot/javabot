450c36e6cbcf0f7d4b33e5e9e045ea3c3980fae1
Ваше Имя <you@example.com>
2021-01-26 00:31:21 +0300

    Усовершенствована команда пинга

--- src/main/java/commands/info/PingCommand.java
+++ src/main/java/commands/info/PingCommand.java
@@ -1,5 +1,6 @@
 package commands.info;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -8,8 +9,11 @@ public class PingCommand extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent msg_event) {
-        if (msg_event.getMessage().getContentRaw().equals("..ping")){   
-            msg_event.getMessage().reply("Pong!").queue();
+        if (msg_event.getMessage().getContentRaw().equals("..ping")){
+            EmbedBuilder pingEmbed = new EmbedBuilder();
+            pingEmbed.setTitle("Пинг бота");
+            pingEmbed.setDescription(String.valueOf(msg_event.getJDA().getResponseTotal()));
+            msg_event.getMessage().reply(pingEmbed.build()).queue();
         }
     }
 
