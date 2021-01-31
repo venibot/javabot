46d6c8bae1ec4288908dc3235c22a52c859686d8
Ваше Имя <you@example.com>
2021-01-31 20:43:42 +0300

    Добавлен автоматический загрузчик команд, подчищены импорты

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
-import api.SupportServer;
+import api.models.command.Command;
+import api.models.command.CommandHandler;
 import api.utils.Config;
 import events.*;
 import net.dv8tion.jda.api.AccountType;
@@ -7,6 +8,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import javax.security.auth.login.LoginException;
+import java.io.File;
 
 public class Main {
 
@@ -18,9 +20,25 @@ public class Main {
         }
         builder.setToken(Config.BOT_CONFIG.get("token"));
         JDA bot = builder.build();
+        loadCommands("src/main/java/commands", "commands");
         bot.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
         bot.getPresence().setActivity(Activity.playing("изучение java"));
         bot.addEventListener(new MessageReceived());
+    }
+
+    public static void loadCommands(String path, String commands_package) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        commands_package += ".";
+        File dir = new File(path);
+        for (File file: dir.listFiles()) {
+            if (file.isFile() && file.getName().endsWith(".java")){
+                Class cmd = Class.forName(commands_package + file.getName().replace(".java", ""));
+                Command command = (Command) cmd.newInstance();
+                CommandHandler.registerCommand(command);
+            }
+            else if (file.isDirectory()) {
+                loadCommands(file.getPath(), "commands." + file.getName());
+            }
+        }
     }
 
 }
--- src/main/java/events/MessageReceived.java
+++ src/main/java/events/MessageReceived.java
@@ -2,20 +2,10 @@ package events;
 
 import api.models.command.Command;
 import api.models.command.CommandHandler;
-import commands.SayCommand;
-import commands.info.HelpCommand;
-import commands.info.PingCommand;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class MessageReceived extends ListenerAdapter {
-
-
-    public MessageReceived(){
-        CommandHandler.registerCommand(new SayCommand());
-        CommandHandler.registerCommand(new PingCommand());
-        CommandHandler.registerCommand(new HelpCommand());;
-    }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent msg_event) {
