a84395f1754139f7e1e5ec85fd2384c751621dbb
Михаил Баринов <barinovmishka@yandex.ru>
2021-02-19 21:47:00 +0300

    Добавлена отправка статистики бота на мониторинги

--- src/main/java/events/Ready.java
+++ src/main/java/events/Ready.java
@@ -1,5 +1,7 @@
 package events;
 
+import api.APIs.Boticord;
+import api.APIs.SDC;
 import api.Database;
 import api.models.database.User;
 import api.models.exceptions.AlreadyInDatabaseException;
@@ -7,6 +9,8 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.io.IOException;
 
 public class Ready extends ListenerAdapter {
 
@@ -28,6 +32,20 @@ public class Ready extends ListenerAdapter {
                     continue;
                 }
             }
+        }
+
+        Boticord boticord = new Boticord();
+        try {
+            boticord.sendStat(event.getGuildTotalCount(), 1, event.getJDA().getUsers().size());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SDC sdc = new SDC();
+        try {
+            sdc.sendStat(event.getJDA().getSelfUser().getIdLong(), event.getGuildTotalCount(), 1);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         System.out.println(
