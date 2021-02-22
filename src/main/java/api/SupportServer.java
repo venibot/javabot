4d3d16f3caac2910d9489b60dac0ad39b7c0e2d9
respects-in-durka <71569261+respects-in-durka@users.noreply.github.com>
2021-02-22 21:18:02 +0200

    Update SupportServer.java

--- src/main/java/api/SupportServer.java
+++ src/main/java/api/SupportServer.java
@@ -10,77 +10,68 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 
-import java.awt.*;
+public class SupportServer {
 
-public class SupportServer implements api.models.SupportServer {
-
-    @Override
     public Guild getGuild(JDA bot) {
         return bot.getGuildById(759796323569500160L);
     }
 
-    @Override
     public boolean isTester(JDA bot, User user) {
         Guild guild = getGuild(bot);
         Member member = guild.getMember(user);
         try {
-            return member.getRoles().contains(guild.getRoleById(770374723900407888L));
+            return member != null && member.getRoles().contains(guild.getRoleById(770374723900407888L));
         }
         catch (NullPointerException e) {
             return false;
         }
     }
 
-    @Override
     public boolean isSupport(JDA bot, User user) {
         Guild guild = getGuild(bot);
         Member member = guild.getMember(user);
         try {
-            return member.getRoles().contains(guild.getRoleById(770374723900407888L));
+            return member != null && member.getRoles().contains(guild.getRoleById(770374723900407888L));
         }
         catch (NullPointerException e) {
             return false;
         }
     }
 
-    @Override
     public boolean isStaff(JDA bot, User user) {
         Guild guild = getGuild(bot);
         Member member = guild.getMember(user);
         try {
-            return member.getRoles().contains(guild.getRoleById(770374723900407888L));
+            return member != null && member.getRoles().contains(guild.getRoleById(770374723900407888L));
         }
         catch (NullPointerException e) {
             return false;
         }
     }
 
-    @Override
     public boolean isDeveloper(JDA bot, User user) {
         Guild guild = getGuild(bot);
         Member member = guild.getMember(user);
         try {
-            return member.getRoles().contains(guild.getRoleById(770374723900407888L));
+            return member != null && member.getRoles().contains(guild.getRoleById(770374723900407888L));
         }
         catch (NullPointerException e) {
             return false;
         }
     }
 
-    @Override
-    public boolean isDonator(JDA bot, User user) {
+    public boolean isDonater(JDA bot, User user) {
         Guild guild = getGuild(bot);
         Member member = guild.getMember(user);
         try {
-            return member.getRoles().contains(guild.getRoleById(770374723900407888L));
+            return member != null && member.getRoles().contains(guild.getRoleById(770374723900407888L));
         }
         catch (NullPointerException e) {
             return false;
         }
     }
 
-    @Override
-    public void sendError(JDA bot, Exception error) {
+    public void sendError(Exception error) {
         WebhookClient errorWebhook = WebhookClient.withUrl(Config.BOT_CONFIG.get("errorWebhookUrl"));
         WebhookEmbed.EmbedTitle title = new WebhookEmbed.EmbedTitle("Непредвиденная ошибка!", null);
         WebhookEmbed errorEmbed = new WebhookEmbedBuilder()
@@ -91,15 +82,27 @@ public class SupportServer implements api.models.SupportServer {
         errorWebhook.send(errorEmbed);
     }
 
-    @Override
-    public void sendGulag(JDA bot, Guild guild, boolean left, User gulagger) {
+    public void sendGulag(JDA bot, Guild guild, boolean left, User gulagUser) {
         TextChannel channel = bot.getTextChannelById(812407745939505273L);
-        channel.sendMessage(new BasicEmbed("info")
-                .setTitle("Сервер " + guild.getName() + " отправлен в гулаг")
-                .setDescription((left ? "Я ливнул с сервера." : "Я не ливал с сервера.") + "\n"
-                + "Отправил в гулаг " + gulagger.getAsTag())
-                .setFooter("ID " + guild.getId())
-                .build()).queue();
+        if (channel != null) {
+            channel.sendMessage(new BasicEmbed("info")
+                    .setTitle("Сервер " + guild.getName() + " отправлен в гулаг")
+                    .setDescription((left ? "Я ливнул с сервера." : "Я не ливал с сервера.") + "\n"
+                    + "Отправил в гулаг " + gulagUser.getAsTag())
+                    .setFooter("ID " + guild.getId())
+                    .build()).queue();
+        }
+    }
+
+    public void sendGulagAttempt(JDA bot, Guild guild, User adder) {
+        TextChannel channel = bot.getTextChannelById(812407745939505273L);
+        if (channel != null) {
+            channel.sendMessage(new BasicEmbed("info")
+                    .setTitle("Меня попытались добавить на сервер " + guild.getName() + ", но он в гулаге")
+                    .setDescription("Попытался добавить " + (adder != null ? adder.getAsTag() : "Пользователя определить не удалось"))
+                    .setFooter("ID " + guild.getId())
+                    .build()).queue();
+        }
     }
 
 }
