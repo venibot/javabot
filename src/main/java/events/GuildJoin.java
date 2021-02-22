2c188cc4404cdec93032f95cc1266b31df4cc24d
respects-in-durka <71569261+respects-in-durka@users.noreply.github.com>
2021-02-22 21:19:57 +0200

    Create GuildJoin.java

---
+++ src/main/java/events/GuildJoin.java
@@ -0,0 +1,33 @@
+import api.Database;
+import api.SupportServer;
+import api.models.exceptions.AlreadyInDatabaseException;
+import api.utils.database.models.Guild;
+import net.dv8tion.jda.api.audit.ActionType;
+import net.dv8tion.jda.api.audit.AuditLogEntry;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class GuildJoin extends ListenerAdapter {
+
+    @Override
+    public void onGuildJoin(GuildJoinEvent joinEvent) {
+        Database db = new Database();
+        try {
+            db.addGuild(new Guild(joinEvent.getGuild().getIdLong()));
+        } catch (AlreadyInDatabaseException ignored) {}
+        User adder = null;
+        if (db.getGuildByID(joinEvent.getGuild().getIdLong()).isInGulag()) {
+            for (AuditLogEntry entry : joinEvent.getGuild().retrieveAuditLogs().limit(5).type(ActionType.BOT_ADD)) {
+                if (entry.getTargetIdLong() == joinEvent.getJDA().getSelfUser().getIdLong()) {
+                    adder = entry.getUser();
+                    break;
+                }
+            }
+            joinEvent.getGuild().leave().queue();
+            SupportServer supportServer = new SupportServer();
+            supportServer.sendGulagAttempt(joinEvent.getJDA(), joinEvent.getGuild(), adder);
+        }
+    }
+
+}
