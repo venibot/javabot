c3526e30b1f0dcde2767fcecd032a48e79cd7c20
respects-in-durka <71569261+respects-in-durka@users.noreply.github.com>
2021-02-22 21:13:27 +0200

    Update Command.java

--- src/main/java/api/models/command/Command.java
+++ src/main/java/api/models/command/Command.java
@@ -12,13 +12,4 @@ public interface Command {
         return getClass().getAnnotation(DiscordCommand.class);
     }
 
-    default String getAttributeByKey(String key) {
-        if (!hasAttribute(key)) return null;
-        return Arrays.stream(getCommandData().attributes()).filter(c -> c.key().equals(key)).findFirst().get().value();
-    }
-
-    default boolean hasAttribute(String key) {
-        return Arrays.stream(getCommandData().attributes()).anyMatch(c -> c.key().equals(key));
-    }
-
 }
