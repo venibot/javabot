13f9fe39cebe075dbc41f39325b92367ea999638
respects-in-durka <71569261+respects-in-durka@users.noreply.github.com>
2021-02-22 21:12:55 +0200

    Delete CommandException.java
    нафиг это нада??

--- src/main/java/api/models/command/CommandException.java
+++
@@ -1,9 +0,0 @@
-package api.models.command;
-
-public class CommandException extends RuntimeException {
-    private static final long serialVersionUID = -3419515084851063729L;
-
-    CommandException(Throwable cause) {
-        super(cause);
-    }
-}
