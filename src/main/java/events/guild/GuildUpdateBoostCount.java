092e1471c2dcb65ea84ee86ad28d0c69d45fb7c7
Михаил Баринов <barinovmishka@yandex.ru>
2021-03-17 21:46:14 +0300

    Добавлены логи изменения количества бустов сервера

---
+++ src/main/java/events/guild/GuildUpdateBoostCount.java
@@ -0,0 +1,23 @@
+package events.guild;
+
+import api.BasicEmbed;
+import api.utils.GetLogChannel;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.guild.update.GuildUpdateBoostCountEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class GuildUpdateBoostCount extends ListenerAdapter {
+
+    @Override
+    public void onGuildUpdateBoostCount(GuildUpdateBoostCountEvent updateBoostCountEvent) {
+        TextChannel logChannel = GetLogChannel.getChannel(updateBoostCountEvent.getGuild(), "guildUpdate");
+        if (logChannel != null) {
+            BasicEmbed logEmbed = new BasicEmbed("info");
+            logEmbed.setTitle("Изменено количество бустов сервера");
+            logEmbed.addField("Старое количество бустов", String.valueOf(updateBoostCountEvent.getOldBoostCount()));
+            logEmbed.addField("Новое количество бустов", String.valueOf(updateBoostCountEvent.getNewBoostCount()));
+            logChannel.sendMessage(logEmbed.build()).queue();
+        }
+    }
+
+}
