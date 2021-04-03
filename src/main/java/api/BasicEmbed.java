5fc816740dada513983c87d6895f1fcfb5cb8c2e
Михаил Баринов <barinovmishka@yandex.ru>
2021-04-04 00:24:54 +0300

    Исправлен показ времени в футере эмбеда

--- src/main/java/api/BasicEmbed.java
+++ src/main/java/api/BasicEmbed.java
@@ -3,14 +3,15 @@ package api;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 import java.awt.*;
-import java.time.LocalDateTime;
+import java.time.*;
+import java.util.Date;
 
 public class BasicEmbed extends EmbedBuilder {
 
     public BasicEmbed() {
         this.setFooter("dmemsm#1706 Все права закодированы", "https://images-ext-2.discordapp.net/external/L7-DkCwFDp1MUNWTxjgvInsppR1UNGtOVYw91im7HfU/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/453179077294161920/c4064a11220a15ff053ce7f2b674de3b.webp");
         this.setAuthor("VeniBot#0739", "https://images-ext-1.discordapp.net/external/mQoekC9Fhw-04HJPX8Wm8o9-ICgn3c-grwge4uVNxXw/https/cdn.discordapp.com/avatars/728030884179083354/eab5bec7816f46847d0a85915ba74d7c.png");
-        this.setTimestamp(LocalDateTime.now());
+        this.setTimestamp(new Date().toInstant().atOffset(ZoneOffset.UTC));
     }
 
     public BasicEmbed(String type) {
@@ -27,7 +28,7 @@ public class BasicEmbed extends EmbedBuilder {
         }
         this.setFooter("dmemsm#1706 Все права закодированы", "https://images-ext-2.discordapp.net/external/L7-DkCwFDp1MUNWTxjgvInsppR1UNGtOVYw91im7HfU/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/453179077294161920/c4064a11220a15ff053ce7f2b674de3b.webp");
         this.setAuthor("VeniBot#0739", "https://images-ext-1.discordapp.net/external/mQoekC9Fhw-04HJPX8Wm8o9-ICgn3c-grwge4uVNxXw/https/cdn.discordapp.com/avatars/728030884179083354/eab5bec7816f46847d0a85915ba74d7c.png");
-        this.setTimestamp(LocalDateTime.now());
+        this.setTimestamp(new Date().toInstant().atOffset(ZoneOffset.UTC));
     }
 
     public BasicEmbed(String description, String type) {
@@ -45,7 +46,7 @@ public class BasicEmbed extends EmbedBuilder {
         }
         this.setFooter("dmemsm#1706 Все права закодированы", "https://images-ext-2.discordapp.net/external/L7-DkCwFDp1MUNWTxjgvInsppR1UNGtOVYw91im7HfU/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/453179077294161920/c4064a11220a15ff053ce7f2b674de3b.webp");
         this.setAuthor("VeniBot#0739", "https://images-ext-1.discordapp.net/external/mQoekC9Fhw-04HJPX8Wm8o9-ICgn3c-grwge4uVNxXw/https/cdn.discordapp.com/avatars/728030884179083354/eab5bec7816f46847d0a85915ba74d7c.png");
-        this.setTimestamp(LocalDateTime.now());
+        this.setTimestamp(new Date().toInstant().atOffset(ZoneOffset.UTC));
     }
 
     public BasicEmbed(String title, String description, String type) {
@@ -64,7 +65,7 @@ public class BasicEmbed extends EmbedBuilder {
         }
         this.setFooter("dmemsm#1706 Все права закодированы", "https://images-ext-2.discordapp.net/external/L7-DkCwFDp1MUNWTxjgvInsppR1UNGtOVYw91im7HfU/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/453179077294161920/c4064a11220a15ff053ce7f2b674de3b.webp");
         this.setAuthor("VeniBot#0739", "https://cdn.discordapp.com/avatars/728030884179083354/eab5bec7816f46847d0a85915ba74d7c.png");
-        this.setTimestamp(LocalDateTime.now());
+        this.setTimestamp(new Date().toInstant().atOffset(ZoneOffset.UTC));
     }
 
     public BasicEmbed addField(String name, String value) {
