1e6620b64dce3173765daf7e78a734abce43a528
Михаил Баринов <barinovmishka@yandex.ru>
2021-04-03 23:38:41 +0300

    Исправлена возможность пингов через say

--- src/main/java/commands/SayCommand.java
+++ src/main/java/commands/SayCommand.java
@@ -3,7 +3,9 @@ package commands;
 import api.BasicEmbed;
 import api.models.command.Command;
 import api.models.command.DiscordCommand;
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 @DiscordCommand(name = "say", aliases = {"сказать", "скажи"}, arguments = 1, group = "Утилиты")
@@ -15,7 +17,11 @@ public class SayCommand implements Command {
             if (msg_event.getGuild().getSelfMember().hasPermission(Permission.MESSAGE_MANAGE)) {
                 msg_event.getMessage().delete().queue();
             }
-            msg_event.getChannel().sendMessage(String.join(" ", arguments)).queue();
+            MessageBuilder builder = new MessageBuilder(String.join(" ", arguments));
+            if (!msg_event.getMember().hasPermission(Permission.MESSAGE_MENTION_EVERYONE)) {
+                builder.denyMentions(Message.MentionType.EVERYONE, Message.MentionType.HERE, Message.MentionType.ROLE, Message.MentionType.USER);
+            }
+            msg_event.getChannel().sendMessage(builder.build()).queue();
         } else {
             BasicEmbed errorEmbed = new BasicEmbed("error");
             errorEmbed.setDescription("О, а можете меня научить ничего не говорить по команде?");
