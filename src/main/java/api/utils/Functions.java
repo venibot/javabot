267dd5cdc18b0bddfb9c09dee693a2a88bcdabec
Михаил Баринов <barinovmishka@yandex.ru>
2021-04-08 00:39:14 +0300

    Добавлены команды для работы с базой данных

--- src/main/java/api/utils/Functions.java
+++ src/main/java/api/utils/Functions.java
@@ -1,5 +1,7 @@
 package api.utils;
 
+import java.util.List;
+
 public class Functions {
 
     public static void debug(Object[] array) {
@@ -10,6 +12,46 @@ public class Functions {
         output = output.replaceAll(", $", "");
         output += "]";
         System.out.println(output);
+    }
+
+    public static void debug(List array) {
+        String output = "[";
+        for (Object object: array) {
+            output += object + ", ";
+        }
+        output = output.replaceAll(", $", "");
+        output += "]";
+        System.out.println(output);
+    }
+
+    public static String debug(Object[] array, boolean toReturn) {
+        String output = "[";
+        for (Object object: array) {
+            output += object + ", ";
+        }
+        output = output.replaceAll(", $", "");
+        output += "]";
+        if (toReturn) {
+            return output;
+        } else {
+            System.out.println(output);
+            return null;
+        }
+    }
+
+    public static String debug(List array, boolean toReturn) {
+        String output = "[";
+        for (Object object: array) {
+            output += object + ", ";
+        }
+        output = output.replaceAll(", $", "");
+        output += "]";
+        if (toReturn) {
+            return output;
+        } else {
+            System.out.println(output);
+            return null;
+        }
     }
 
 }
---
+++ src/main/java/commands/owner/DatabaseCommand.java
@@ -0,0 +1,55 @@
+package commands.owner;
+
+import api.models.command.Command;
+import api.models.command.DiscordCommand;
+import api.utils.Config;
+import api.utils.Functions;
+import com.google.gson.Gson;
+import com.mongodb.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.List;
+
+@DiscordCommand(name = "database", description = "Работа с базой данных", aliases = {"db", "database", "датабазе"}, group = "Владельцу",
+                hidden = true, accessLevel = 4, arguments = 4)
+public class DatabaseCommand implements Command {
+
+    @Override
+    public void doCommand(MessageReceivedEvent msg_event, String[] arguments) {
+        if (arguments[0].equals("get")) {
+            String collection = arguments[1];
+            BasicDBObject query = new Gson().fromJson((arguments.length > 3 ? arguments[2] + arguments[3] : arguments[2]), BasicDBObject.class);
+            MongoClient client = new MongoClient(new MongoClientURI(Config.DB_CONFIG.get("uri")));
+            DB database = client.getDB(Config.DB_CONFIG.get("db"));
+            List<DBObject> result = database.getCollection(collection).find(query).toArray();
+            String resultToSend = "```" + Functions.debug(result, true) + "```";
+            if (resultToSend.length() > 2000) {
+                msg_event.getChannel().sendMessage("Результат длиннее 2000 символов, вот его укороченный вариант").queue(response -> {
+                    response.editMessage(resultToSend.substring(0, 1996) + "```").queue();
+                });
+            } else {
+                msg_event.getChannel().sendMessage(resultToSend).queue();
+            }
+            client.close();
+        }
+        if (arguments[0].equals("delete")) {
+            String collection = arguments[1];
+            BasicDBObject query = new Gson().fromJson((arguments.length > 3 ? arguments[2] + arguments[3] : arguments[2]), BasicDBObject.class);
+            MongoClient client = new MongoClient(new MongoClientURI(Config.DB_CONFIG.get("uri")));
+            DB database = client.getDB(Config.DB_CONFIG.get("db"));
+            database.getCollection(collection).remove(query);
+            msg_event.getMessage().addReaction("✅").queue();
+            client.close();
+        }
+        if (arguments[0].equals("update")) {
+            String collection = arguments[1];
+            BasicDBObject query = new Gson().fromJson((arguments[2]), BasicDBObject.class);
+            BasicDBObject update = new Gson().fromJson((arguments[4]), BasicDBObject.class);
+            MongoClient client = new MongoClient(new MongoClientURI(Config.DB_CONFIG.get("uri")));
+            DB database = client.getDB(Config.DB_CONFIG.get("db"));
+            database.getCollection(collection).update(query, update);
+            msg_event.getMessage().addReaction("✅").queue();
+            client.close();
+        }
+    }
+}
