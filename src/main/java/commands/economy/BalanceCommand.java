7924c2d6ef3bc32754e339e2ad99a7d39fb90301
Михаил Баринов <barinovmishka@yandex.ru>
2021-04-08 23:29:08 +0300

    Фиксы команды баланса и работы с БД...

---
+++ src/main/java/commands/economy/BalanceCommand.java
@@ -0,0 +1,35 @@
+package commands.economy;
+
+import api.BasicEmbed;
+import api.Database;
+import api.models.command.Command;
+import api.models.command.DiscordCommand;
+import api.models.exceptions.MemberNotFoundException;
+import api.utils.Converters;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+@DiscordCommand(name = "balance", description = "Узнать баланс пользователя", aliases = {"bal", "баланс", "bal"},
+                usage = "[Пользователь]", group = "Экономика", arguments = 1)
+public class BalanceCommand implements Command {
+
+    @Override
+    public void doCommand(MessageReceivedEvent msg_event, String[] arguments) {
+        Member member = null;
+        if (arguments.length == 0) {
+            member = msg_event.getMember();
+        } else {
+            try {
+                member = Converters.getMember(msg_event.getGuild(), arguments[0]);
+            } catch (MemberNotFoundException e) {
+                BasicEmbed errorEmbed = new BasicEmbed("error", "Указанный пользователь не найден");
+                msg_event.getChannel().sendMessage(errorEmbed.build()).queue();
+                return;
+            }
+        }
+        Database db = new Database();
+        BasicEmbed infoEmbed = new BasicEmbed("info", "Баланс пользователя " + member.getEffectiveName()
+                + " составляет " + db.getUserByID(member.getIdLong(), msg_event.getGuild().getIdLong()).getBalance());
+        msg_event.getChannel().sendMessage(infoEmbed.build()).queue();
+    }
+}
--- src/main/java/commands/owner/DatabaseCommand.java
+++ src/main/java/commands/owner/DatabaseCommand.java
@@ -6,12 +6,13 @@ import api.utils.Config;
 import api.utils.Functions;
 import com.google.gson.Gson;
 import com.mongodb.*;
+import com.mongodb.client.model.DBCollectionUpdateOptions;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.List;
 
 @DiscordCommand(name = "database", description = "Работа с базой данных", aliases = {"db", "database", "датабазе"}, group = "Владельцу",
-                hidden = true, accessLevel = 4, arguments = 4)
+                hidden = true, accessLevel = 4, arguments = 5)
 public class DatabaseCommand implements Command {
 
     @Override
@@ -44,10 +45,11 @@ public class DatabaseCommand implements Command {
         if (arguments[0].equals("update")) {
             String collection = arguments[1];
             BasicDBObject query = new Gson().fromJson((arguments[2]), BasicDBObject.class);
-            BasicDBObject update = new Gson().fromJson((arguments[4]), BasicDBObject.class);
+            BasicDBObject update = new Gson().fromJson((arguments[3]), BasicDBObject.class);
+            DBCollectionUpdateOptions options = new Gson().fromJson(arguments[4], DBCollectionUpdateOptions.class);
             MongoClient client = new MongoClient(new MongoClientURI(Config.DB_CONFIG.get("uri")));
             DB database = client.getDB(Config.DB_CONFIG.get("db"));
-            database.getCollection(collection).update(query, update);
+            database.getCollection(collection).update(query, update, options);
             msg_event.getMessage().addReaction("✅").queue();
             client.close();
         }
