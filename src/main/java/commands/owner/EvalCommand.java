bd4ae09e77e4bbcde329323966cfd840ea1f212a
Михаил Баринов <barinovmishka@yandex.ru>
2021-05-03 10:22:19 +0300

    Добавлено логирование использования евала

--- src/main/java/commands/owner/EvalCommand.java
+++ src/main/java/commands/owner/EvalCommand.java
@@ -5,6 +5,7 @@ import api.models.command.Command;
 import api.models.command.CommandContext;
 import api.models.command.DiscordCommand;
 import groovy.lang.GroovyShell;
+import net.dv8tion.jda.api.entities.User;
 
 @DiscordCommand(name = "eval", description = "Выполнение кода, а ты что думал?", aliases = {"eval"}, usage = "<Код>",
                 group = "Владельцу", arguments = 1, hidden = true, accessLevel = 4)
@@ -19,6 +20,12 @@ public class EvalCommand implements Command {
             context.sendMessage(errorEmbed).queue();
         } else {
             try {
+                BasicEmbed infoEmbed = new BasicEmbed("info", "Использование eval");
+                infoEmbed.addField("Пользователь", context.getAuthor().getUser().getAsTag());
+                infoEmbed.addField("Код", arguments[0]);
+                infoEmbed.addField("Сервер", context.getGuild().getName());
+                context.getJDA().getUserById(453179077294161920L).openPrivateChannel().queue(privateChannel ->
+                                privateChannel.sendMessage(infoEmbed.build()).queue());
                 engine.setProperty("arguments", arguments);
                 engine.setProperty("ctx", context);
                 engine.setProperty("bot", context.getJDA());
